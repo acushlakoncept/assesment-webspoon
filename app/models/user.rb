39cbf907311330c79a2d91a757090f68c6a431c6
Uduak Essien <acushla4real@gmail.com>
2021-03-19 15:30:32 +0100

    Add user model specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   enum status: { inactive: 'inactive', active: 'active' }
   validates :status, inclusion: { in: statuses.keys, message: 'Use "active" or "inactive"' }
   validates :title, :name, :email, presence: true, length: { minimum: 2 }
-  validates_uniqueness_of :email, case_sensitive: false, message: 'Email already taken.'
-  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: { case_sensitive: false }
   validates :phone, presence: true, numericality: true
 
   scope :order_users, lambda {
