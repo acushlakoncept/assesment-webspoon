c213cbc89c24ab008549f9a2300a6c2c567b33be
Uduak Essien <acushla4real@gmail.com>
2021-03-19 12:07:20 +0100

    Add search method to user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,26 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
-  before_action :set_user, only: %i[ show edit update destroy ]
+  before_action :set_user, only: %i[show edit update destroy]
 
-  # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.search(params[:search]).order_users
   end
 
-  # GET /users/1 or /users/1.json
-  def show
-  end
+  def show; end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,11 +29,10 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: "User was successfully updated." }
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,23 +41,21 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1 or /users/1.json
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: "User was successfully destroyed." }
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :email, :title, :phone, :status)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :title, :phone, :status)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,24 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-    enum status: { inactive: 'inactive', active: 'active' }
-    validates :status, inclusion: { in: statuses.keys, message: 'Use "active" or "inactive"' }
-    validates :title, :name, :email, presence: true, length: { minimum: 2 }
-    validates_uniqueness_of :email, case_sensitive: false, message: 'Email already taken.'
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
-    validates :phone, presence: true, numericality: true
+  enum status: { inactive: 'inactive', active: 'active' }
+  validates :status, inclusion: { in: statuses.keys, message: 'Use "active" or "inactive"' }
+  validates :title, :name, :email, presence: true, length: { minimum: 2 }
+  validates_uniqueness_of :email, case_sensitive: false, message: 'Email already taken.'
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, presence: true, numericality: true
+
+  scope :order_users, lambda {
+                        order(updated_at: :desc).order(name: :asc).order(email: :asc).order(title: :asc).order(phone: :asc).order(status: :asc)
+                      }
+
+  def self.search(search)
+    if search
+      User.where('lower(name) LIKE ? OR lower(email) LIKE ? OR lower(status) LIKE ? OR phone = ?',
+                 "%#{search.downcase}%", "%#{search.downcase}%", search.downcase.to_s, search.to_i)
+
+    else
+      User.all
+    end
+  end
 end
