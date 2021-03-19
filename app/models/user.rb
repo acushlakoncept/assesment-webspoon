73e2099ed4e9610e573b49504f9dfd7846ef667d
Uduak Essien <acushla4real@gmail.com>
2021-03-19 09:43:29 +0100

    Update user form to use select

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     enum status: { inactive: 'inactive', active: 'active' }
     validates :status, inclusion: { in: statuses.keys, message: 'Use "active" or "inactive"' }
-    validates :title, :name, :email, presence: true, length: { minimum: 3 }
+    validates :title, :name, :email, presence: true, length: { minimum: 2 }
     validates_uniqueness_of :email, case_sensitive: false, message: 'Email already taken.'
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
     validates :phone, presence: true, numericality: true
