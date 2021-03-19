da8a65df84e77d5e021a46a68ffe685d7e9e3c49
Uduak Essien <acushla4real@gmail.com>
2021-03-19 09:31:30 +0100

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    enum status: { inactive: 'inactive', active: 'active' }
+    validates :status, inclusion: { in: statuses.keys, message: 'Use "active" or "inactive"' }
+    validates :title, :name, :email, presence: true, length: { minimum: 3 }
+    validates_uniqueness_of :email, case_sensitive: false, message: 'Email already taken.'
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
+    validates :phone, presence: true, numericality: true
 end
